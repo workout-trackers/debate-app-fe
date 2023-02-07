fce76fea01724eb487b2720e2f288621ef78e409
MatthewPress <press.matt14@gmail.com>
2023-02-07 10:29:22 -0700

    refactor: make navbar component work with new styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,23 +3,33 @@ import styles from '@/styles/NavBar.module.css';
 import { User } from '@/interfaces';
 
 interface NavBarProps {
-  user: User;
+  user?: User;
 }
 
 export default function NavBar({ user }: NavBarProps) {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <nav>
+    <nav className={styles.navbar}>
       <button
         className={styles.navbarButton}
         onClick={() => (navOpen ? setNavOpen(false) : setNavOpen(true))}
       >
-        <div className={styles.lineOne}></div>
-        <div className={styles.lineTwo}></div>
-        <div className={styles.lineThree}></div>
+        <div
+          className={`${styles.lineOne} ${navOpen ? styles.active : null}`}
+        ></div>
+        <div
+          className={`${styles.lineTwo} ${navOpen ? styles.active : null}`}
+        ></div>
+        <div
+          className={`${styles.lineThree} ${navOpen ? styles.active : null}`}
+        ></div>
       </button>
-      <ul className={`${styles.navbarList} ${navOpen ? styles.navOpen : null}`}>
+      <ul
+        className={`${styles.navbarList} ${
+          navOpen ? styles.navbarListVisible : null
+        }`}
+      >
         <li className={styles.navbarListItem}>About</li>
         <li className={styles.navbarListItem}>All Boards</li>
         {user ? <li className={styles.navbarListItem}>Your Boards</li> : null}
