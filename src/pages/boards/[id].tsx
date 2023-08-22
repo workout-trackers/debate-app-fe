45f1aafdd14fe4da2dee63eb8c33f5c96ff805a3
MatthewPress <press.matt14@gmail.com>
2023-08-22 11:08:49 -0500

    feat: create initial board page layout

--- src/pages/boards/[id].tsx
+++ src/pages/boards/[id].tsx
@@ -1,16 +1,17 @@
-import Header from '@/components/Header';
-import NavBar from '@/components/NavBar';
-import InfoSection from '@/components/InfoSection';
-import styles from '@/styles/Home.module.css';
+import Layout from '@/layouts/layout';
+import styles from '@/styles/Board.module.css';
+
+import users from '../../data/users.json';
 
 export default function Board() {
   return (
-    <>
-      <main className="main">
-        <Header />
-        <NavBar />
-        <InfoSection />
-      </main>
-    </>
+    <Layout user={users[0]}>
+      <section className={styles.board}>
+        <article className={styles.userA}>{/* User 1 */}</article>
+        <section className={styles.topic}>{/* Topic */}</section>
+        {/* Categories */}
+        <article className={styles.userB}>{/* User 2 */}</article>
+      </section>
+    </Layout>
   );
 }
