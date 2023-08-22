3a816e391eeb5fdc4522e5a7eee5762a7fa1dccc
MatthewPress <press.matt14@gmail.com>
2023-08-22 12:34:52 -0500

    chore: add user and topic data for a board

--- src/pages/boards/[id].tsx
+++ src/pages/boards/[id].tsx
@@ -2,15 +2,24 @@ import Layout from '@/layouts/layout';
 import styles from '@/styles/Board.module.css';
 
 import users from '../../data/users.json';
+import topics from '../../data/topics.json';
+import usersTopics from '../../data/usersTopics.json';
 
 export default function Board() {
   return (
     <Layout user={users[0]}>
       <section className={styles.board}>
-        <article className={styles.userA}>{/* User 1 */}</article>
-        <section className={styles.topic}>{/* Topic */}</section>
+        <article className={`${styles.userBoardBall} ${styles.userA}`}>
+          <h4>{usersTopics[0].position}</h4>
+        </article>
+        <section className={styles.topic}>
+          <h2>{topics[0].title}</h2>
+          <p>{topics[0].description}</p>
+        </section>
         {/* Categories */}
-        <article className={styles.userB}>{/* User 2 */}</article>
+        <article className={`${styles.userBoardBall} ${styles.userB}`}>
+          <h4>{usersTopics[1].position}</h4>
+        </article>
       </section>
     </Layout>
   );
