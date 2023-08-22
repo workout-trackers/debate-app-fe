07bfe49157edb943241144d5aa7002cd62d1fb14
MatthewPress <press.matt14@gmail.com>
2023-08-22 11:05:05 -0500

    feat: create standardized layout for site

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,26 @@
+import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
+import UserSection from "@/components/UserSection";
+import InfoSection from "@/components/InfoSection";
+import { ReactNode } from 'react';
+import { User } from "@/interfaces";
+
+interface LayoutProps {
+  user?: User;
+  children: ReactNode;
+}
+
+export default function Layout({ user, children }: LayoutProps) {
+  return (
+    <main className="main">
+      <Header />
+      <NavBar user={user} />
+      { children }
+      { 
+        user
+        ? <UserSection />
+        : <InfoSection />
+      }
+    </main>
+  );
+}
